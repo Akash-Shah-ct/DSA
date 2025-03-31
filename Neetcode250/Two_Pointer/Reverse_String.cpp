280f5550b03885fd1dc92ac4d8ae28d8d5065bc4
Akash-Shah-ct <shahakashct@gmail.com>
2025-03-31 20:27:56 +0530

    Add recursive Solution with O(n) Space complexity

--- Neetcode250/Two_Pointer/Reverse_String.cpp
+++ Neetcode250/Two_Pointer/Reverse_String.cpp
@@ -15,13 +15,13 @@
 
 class Solution {
 public:
-    void reverseString(vector<char>& s) {
-        /* Using Two pointer approach. 
-         * The idea is to use two pointers at start and end
-         * and do the reversal of string.
-         * After each itteration, move start pointer forward
-         * and end pointer backward until the meet the midpoint of the array.
-         */
+    /* Using Two pointer approach. 
+     * The idea is to use two pointers at start and end
+     * and do the reversal of string.
+     * After each itteration, move start pointer forward
+     * and end pointer backward until the meet the midpoint of the array.
+     */
+    void reverseStringUsingTwoPointer(vector<char>& s) {
         int start_index = 0;
         int end_index = s.size() - 1;
         while (start_index < end_index) {
@@ -30,6 +30,24 @@ public:
             end_index--;
         }
     }
+
+    /*
+     * Using Recursive approach.
+     * This is effectively two pointer apporoach only,
+     * but in recursive manner. This should be not be optimal, since we
+     * are not using O(1) space complexity, and rather using O(n) space.
+     */
+    void reverseStringIndexWise (vector<char>& s, int start, int end) {
+        if (start >= end) return;  // Base case is when we have reached midpoint of the array.
+        swap(s[start],s[end]);
+        reverseStringIndexWise(s,start + 1,end - 1);
+    }
+    void reverseStringUsingRecursion(vector<char>& s) {
+        reverseStringIndexWise (s, 0, s.size() - 1);
+    }
 };
+
+
+
 
 
